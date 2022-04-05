55761ea3c8dba79a8ce01ae2aa78afa784824edd
kim0yeeun <ab3s_@naver.com>
2022-04-05 11:03:42 +0900

    css,html 클래스이름수정

---
+++ team2-sample/assets/res/www/js/mcore.extends.js
@@ -0,0 +1,29 @@
+
+
+/*
+공통 라이브러리는 importFiles 배열에 선언한다.
+선언된 라이브러리들은 html파일에서 
+<script src="../js/mcore.min.js"></script>
+가 추가된 곳으로 자동 임포트된다.
+*/
+
+
+(function(window, undefined) {
+
+var 
+thisFileName = "mcore.extends.js",
+
+importFiles = [
+	"jquery-3.6.0.min.js",
+	"muikit-1.0.0.min.js",
+	"wnInterface.extends.js",
+  "common/ui.js",
+  "common/definition.js",
+  "common/util.js",
+  "common/setup.js",
+  "common/MAPI.js"
+];
+
+M.ScriptLoader.writeScript( importFiles, M.ScriptLoader.scriptPath(thisFileName) );
+
+})(window);
