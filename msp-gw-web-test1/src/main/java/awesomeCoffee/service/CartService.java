3e96b147ea40ce11af334c2acca444fcb9896446
kim0yeeun <ab3s_@naver.com>
2022-04-07 15:53:27 +0900

    기맹_cart

---
+++ msp-gw-web-test1/src/main/java/awesomeCoffee/service/CartService.java
@@ -0,0 +1,71 @@
+package awesomeCoffee.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import awesomeCoffee.dto.CartDTO;
+import awesomeCoffee.dto.WishlistDTO;
+
+@Service
+public class CartService {
+	private Logger logger = LoggerFactory.getLogger(CartService.class);
+
+	@Autowired(required = true)
+	@Qualifier("sqlSession_sample")
+	private SqlSession sqlSession;
+
+	@Autowired(required = true)
+	@Qualifier("transactionManager_sample")
+	private DataSourceTransactionManager transactionManager_sample;
+	// 장바구니 insert 
+	public int insertCart(Map<String, Object> param) {
+		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+		def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+		TransactionStatus status = transactionManager_sample.getTransaction(def);
+		int result = 0;
+		try {
+			result = sqlSession.insert("Cart.insertCart", param);
+			transactionManager_sample.commit(status);
+			logger.info("========== 장바구니 등록 완료 : {}", result);
+
+		} catch (Exception e) {
+			logger.error("[ERROR] insertUser() Fail : e : {}", e.getMessage());
+			e.printStackTrace();
+			transactionManager_sample.rollback(status);
+		}
+		return result;
+	}
+	// 장바구니 read
+	public List<CartDTO> selectAllCart(String memberNum) {
+		return sqlSession.selectList("Cart.selectAllCart", memberNum);
+	}
+	// 장바구니 delete
+	public int deleteCart(Map<String, Object> param) {
+		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+		def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+		TransactionStatus status = transactionManager_sample.getTransaction(def);
+		int result = 0;
+		try {
+			result = sqlSession.delete("Cart.deleteCart", param);
+			transactionManager_sample.commit(status);
+			logger.info("========== 장바구니 삭제 완료 : {}", result);
+
+		} catch (Exception e) {
+			logger.error("[ERROR] insertUser() Fail : e : {}", e.getMessage());
+			e.printStackTrace();
+			transactionManager_sample.rollback(status);
+		}
+		return result;
+	}
+}
