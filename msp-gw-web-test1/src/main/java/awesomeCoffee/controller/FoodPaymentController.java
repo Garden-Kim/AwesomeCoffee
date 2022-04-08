c54941002267618640a388ef4a794998689dd654
garden-kim <wjddnjs0119@gmail.com>
2022-04-08 21:37:53 +0900

    food_payment 아직안댐 ㅠㅠ

---
+++ msp-gw-web-test1/src/main/java/awesomeCoffee/controller/FoodPaymentController.java
@@ -0,0 +1,68 @@
+package awesomeCoffee.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import awesomeCoffee.dto.FoodDTO;
+import awesomeCoffee.service.FoodPaymentService;
+import awesomeCoffee.service.FoodService;
+import kr.msp.constant.Const;
+
+@Controller
+public class FoodPaymentController {
+
+	private Logger logger = LoggerFactory.getLogger(FoodPaymentController.class);
+
+	@Autowired
+	FoodPaymentService foodPaymentService;
+
+	// 식자재결제 입력
+	@RequestMapping(method = RequestMethod.POST, value = "/api/foodPayment/insert")
+	public ModelAndView insertFoodPayment(HttpServletRequest request, HttpServletResponse response) {
+
+		Map<String, Object> reqHeadMap = (Map<String, Object>) request.getAttribute(Const.HEAD);
+		Map<String, Object> reqBodyMap = (Map<String, Object>) request.getAttribute(Const.BODY);
+		Map<String, Object> responseBodyMap = new HashMap<String, Object>();
+
+		if (reqHeadMap == null) {
+			reqHeadMap = new HashMap<String, Object>();
+		}
+
+		reqHeadMap.put(Const.RESULT_CODE, Const.OK);
+		reqHeadMap.put(Const.RESULT_MESSAGE, Const.SUCCESS);
+
+		logger.info("======================= reqBodyMap : {}", reqBodyMap.toString());
+
+		int result = foodPaymentService.insertFoodPayment(reqBodyMap);
+
+		if (result > 0) {
+			responseBodyMap.put("rsltCode", "0000");
+			responseBodyMap.put("rsltMsg", "Success");
+		} else {
+			responseBodyMap.put("rsltCode", "2003");
+			responseBodyMap.put("rsltMsg", "Data not found.");
+		}
+
+		ModelAndView mv = new ModelAndView("defaultJsonView");
+		mv.addObject(Const.HEAD, reqHeadMap);
+		mv.addObject(Const.BODY, responseBodyMap);
+
+		return mv;
+	}
+
+}
