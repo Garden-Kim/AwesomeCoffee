e498b06e499226f7a568cc3b21f2f3b6d726b67c
kim0yeeun <ab3s_@naver.com>
2022-04-12 16:01:03 +0900

    기맹_회원주문상세페이지

--- msp-gw-web-test1/src/main/java/awesomeCoffee/controller/OrderlistController.java
+++ msp-gw-web-test1/src/main/java/awesomeCoffee/controller/OrderlistController.java
@@ -1,6 +1,7 @@
 package awesomeCoffee.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,18 +15,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import awesomeCoffee.dto.AuthInfo;
 import awesomeCoffee.dto.MemberOrderDTO;
+import awesomeCoffee.dto.MenuCategoryDTO;
 import awesomeCoffee.dto.MenuDTO;
 import awesomeCoffee.dto.OrderlistDTO;
+import awesomeCoffee.dto.PaymentDTO;
 import awesomeCoffee.service.MemberOrderService;
 import awesomeCoffee.service.MemberService;
 import awesomeCoffee.service.MenuService;
 import awesomeCoffee.service.OrderlistService;
+import awesomeCoffee.service.PaymentService;
 import kr.msp.constant.Const;
 
 @Controller
@@ -39,6 +44,8 @@ public class OrderlistController {
 	private MemberService memberService;
 	@Autowired
 	private MenuService menuService;
+	@Autowired
+	private PaymentService paymentService;
 
 	// 주문내역 insert
 	@RequestMapping(method = RequestMethod.POST, value = "/api/orderlist/regist")
@@ -78,74 +85,152 @@ public class OrderlistController {
 	}
 
 	// 주문내역 read 회원
-		@RequestMapping(method = RequestMethod.POST, value = "/api/orderlist/list")
-		public ModelAndView orderlist(HttpSession session, HttpServletRequest request) {
-			Map<String, Object> responseBodyMap = new HashMap<String, Object>();
-			Map<String, Object> reqHeadMap = (Map<String, Object>) request.getAttribute(Const.HEAD);
-			List<Map<String, Object>> orderList = new ArrayList<Map<String, Object>>();
-			List<MenuDTO> menulist = menuService.selectAllMenu();
-			if (reqHeadMap == null) {
-				reqHeadMap = new HashMap<String, Object>();
-			}
+	@RequestMapping(method = RequestMethod.POST, value = "/api/orderlist/list")
+	public ModelAndView orderlist(HttpSession session, HttpServletRequest request) {
+		Map<String, Object> responseBodyMap = new HashMap<String, Object>();
+		Map<String, Object> reqHeadMap = (Map<String, Object>) request.getAttribute(Const.HEAD);
+		List<Map<String, Object>> orderList = new ArrayList<Map<String, Object>>();
+		List<MenuDTO> menulist = menuService.selectAllMenu();
+		if (reqHeadMap == null) {
+			reqHeadMap = new HashMap<String, Object>();
+		}
 
-			reqHeadMap.put(Const.RESULT_CODE, Const.OK);
-			reqHeadMap.put(Const.RESULT_MESSAGE, Const.SUCCESS);
-			AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
-			if (StringUtils.isEmpty(authInfo)) {
-				responseBodyMap.put("rsltCode", "1003");
-				responseBodyMap.put("rsltMsg", "Login required.");
-			} else {
-				String memberNum = memberService.getMemberNum(authInfo.getLoginId());
-				List<MemberOrderDTO> list = memberOrderService.selectAllMemOrder(memberNum);
-				logger.info("======================= responseBodyMap : {}", list.size());
-				
-				for (int i = 0; i < list.size(); i++) {
-					Map<String, Object> map = new HashMap<String, Object>();
-					map.put("takeout", list.get(i).getTakeout());
-					map.put("cookState", list.get(i).getCookState());
-					map.put("orderPrice", list.get(i).getOrderPrice());
-					map.put("orderTime", list.get(i).getOrderTime());
-					map.put("memberNum", list.get(i).getMemberNum());
-					map.put("orderNum", list.get(i).getOrderNum());
-					List<Map<String, Object>> list1 = new ArrayList<Map<String, Object>>();
-					List<OrderlistDTO> goodsList = orderlistService.selectGoodsNums(map);
-					for (int a = 0; a < goodsList.size(); a++) {
-						Map<String, Object> goodsMap = new HashMap<String, Object>();
-						goodsMap.put("goodsNum", goodsList.get(a).getGoodsNum());
-						goodsMap.put("orderPrice", goodsList.get(a).getOrderPrice());
-						goodsMap.put("goodsName", goodsList.get(a).getGoodsName());
-						
-						list1.add(goodsMap);
-					}
-					map.put("list", list1);
-					// title goodsName
-					if (goodsList.size() > 1) {
-						String titleGoodsName = goodsList.get(0).getGoodsName().toString() + " 외 " + (goodsList.size()-1) + "개";
-						map.put("titleGoodsName", titleGoodsName);
-					} else {
-						String titleGoodsName =  goodsList.get(0).getGoodsName().toString();
-						map.put("titleGoodsName", titleGoodsName);
-					}
-						
-					
-					orderList.add(map);
+		reqHeadMap.put(Const.RESULT_CODE, Const.OK);
+		reqHeadMap.put(Const.RESULT_MESSAGE, Const.SUCCESS);
+		AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+		if (StringUtils.isEmpty(authInfo)) {
+			responseBodyMap.put("rsltCode", "1003");
+			responseBodyMap.put("rsltMsg", "Login required.");
+		} else {
+			String memberNum = memberService.getMemberNum(authInfo.getLoginId());
+			List<MemberOrderDTO> list = memberOrderService.selectAllMemOrder(memberNum);
+			logger.info("======================= responseBodyMap : {}", list.size());
+
+			for (int i = 0; i < list.size(); i++) {
+				Map<String, Object> map = new HashMap<String, Object>();
+				map.put("takeout", list.get(i).getTakeout());
+				map.put("cookState", list.get(i).getCookState());
+				map.put("orderPrice", list.get(i).getOrderPrice());
+				map.put("orderTime", list.get(i).getOrderTime());
+				map.put("memberNum", list.get(i).getMemberNum());
+				map.put("orderNum", list.get(i).getOrderNum());
+				List<Map<String, Object>> list1 = new ArrayList<Map<String, Object>>();
+				List<OrderlistDTO> goodsList = orderlistService.selectGoodsNums(map);
+				for (int a = 0; a < goodsList.size(); a++) {
+					Map<String, Object> goodsMap = new HashMap<String, Object>();
+					goodsMap.put("goodsNum", goodsList.get(a).getGoodsNum());
+					goodsMap.put("orderPrice", goodsList.get(a).getOrderPrice());
+					goodsMap.put("goodsName", goodsList.get(a).getGoodsName());
+
+					list1.add(goodsMap);
 				}
-				logger.info("======================= orderList : {}", orderList.toString());
+				map.put("list", list1);
+				// title goodsName
+				if (goodsList.size() > 1) {
+					String titleGoodsName = goodsList.get(0).getGoodsName().toString() + " 외 " + (goodsList.size() - 1)
+							+ "개";
+					map.put("titleGoodsName", titleGoodsName);
+				} else {
+					String titleGoodsName = goodsList.get(0).getGoodsName().toString();
+					map.put("titleGoodsName", titleGoodsName);
+				}
 
-				if (!StringUtils.isEmpty(list)) {
+				orderList.add(map);
+			}
+			logger.info("======================= orderList : {}", orderList.toString());
+
+			if (!StringUtils.isEmpty(list)) {
+				responseBodyMap.put("rsltCode", "0000");
+				responseBodyMap.put("rsltMsg", "Success");
+				responseBodyMap.put("list", orderList);
+			} else {
+				responseBodyMap.put("rsltCode", "2003");
+				responseBodyMap.put("rsltMsg", "Data not found.");
+			}
+		}
+		ModelAndView mv = new ModelAndView("defaultJsonView");
+		mv.addObject(Const.BODY, responseBodyMap);
+		mv.addObject(Const.HEAD, reqHeadMap);
+		return mv;
+	}
+
+	// 주문내역 상세페이지
+	@RequestMapping(method = RequestMethod.POST, value = "/api/orderlist/detail")
+	public ModelAndView orderListDetail(HttpSession session, HttpServletRequest request) {
+		Map<String, Object> responseBodyMap = new HashMap<String, Object>();
+		Map<String, Object> reqBodyMap = (Map<String, Object>) request.getAttribute(Const.BODY);
+		Map<String, Object> reqHeadMap = (Map<String, Object>) request.getAttribute(Const.HEAD);
+		List<Map<String, Object>> orderList = new ArrayList<Map<String, Object>>();
+
+		if (reqHeadMap == null) {
+			reqHeadMap = new HashMap<String, Object>();
+		}
+
+		reqHeadMap.put(Const.RESULT_CODE, Const.OK);
+		reqHeadMap.put(Const.RESULT_MESSAGE, Const.SUCCESS);
+
+		AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+		String memberNum = memberService.getMemberNum(authInfo.getLoginId());
+		reqBodyMap.put("memberNum", memberNum);
+		MemberOrderDTO member = memberOrderService.selectOrderDetail(reqBodyMap);
+		System.out.println(member.getMemberNum().toString());
+		System.out.println(memberNum.toString());
+		if (StringUtils.isEmpty(authInfo)) {
+			responseBodyMap.put("rsltCode", "1003");
+			responseBodyMap.put("rsltMsg", "Login required.");
+		} else {
+			if (!memberNum.equals(member.getMemberNum())) {
+				System.out.println("@!#ER");
+				responseBodyMap.put("rsltCode", "1003");
+				responseBodyMap.put("rsltMsg", "No Permission");
+			} else {
+				List<OrderlistDTO> goodsList = orderlistService.selectGoodsNums(reqBodyMap);
+				// 주문 번호
+				String orderNum = reqBodyMap.get("orderNum").toString();
+				// 주문(결제) 날짜, 결제 수단, 총 주문 가격
+				PaymentDTO dto = paymentService.selectOnePayment(orderNum);
+				Date orderDate = dto.getPaymentDate();
+				String paymentKind = dto.getPaymentKind();
+				String paymentPrice = dto.getPaymentPrice();
+
+				for (int a = 0; a < goodsList.size(); a++) {
+					Map<String, Object> goodsMap = new HashMap<String, Object>();
+					// 상품 이름,번호
+					goodsMap.put("goodsNum", goodsList.get(a).getGoodsNum());
+					goodsMap.put("goodsName", goodsList.get(a).getGoodsName());
+					// 상품 하나당 주문 개수
+					goodsMap.put("orderlistQty", goodsList.get(a).getOrderlistQty());
+					// 상품의 가격
+					goodsMap.put("goodsPrice", goodsList.get(a).getGoodsPrice());
+					// 상품주문개수*상품가격
+					goodsMap.put("price", goodsList.get(a).getPrice());
+
+					orderList.add(goodsMap);
+				}
+				if (!StringUtils.isEmpty(orderList)) {
 					responseBodyMap.put("rsltCode", "0000");
 					responseBodyMap.put("rsltMsg", "Success");
+					responseBodyMap.put("orderDate", orderDate);
+					responseBodyMap.put("paymentKind", paymentKind);
+					responseBodyMap.put("paymentPrice", paymentPrice);
+
+					// 주문한 메뉴 상세리스트
 					responseBodyMap.put("list", orderList);
+
 				} else {
 					responseBodyMap.put("rsltCode", "2003");
 					responseBodyMap.put("rsltMsg", "Data not found.");
 				}
 			}
-			ModelAndView mv = new ModelAndView("defaultJsonView");
-			mv.addObject(Const.BODY, responseBodyMap);
-			mv.addObject(Const.HEAD, reqHeadMap);
-			return mv;
+
 		}
+
+		ModelAndView mv = new ModelAndView("defaultJsonView");
+		mv.addObject(Const.BODY, responseBodyMap);
+		mv.addObject(Const.HEAD, reqHeadMap);
+		return mv;
+	}
+
 	// 주문내역 delete
 	@RequestMapping(method = RequestMethod.POST, value = "/api/orderlist/delete")
 	public ModelAndView orderListDelete(HttpServletRequest request, HttpSession session) {
--- msp-gw-web-test1/src/main/java/awesomeCoffee/dto/OrderlistDTO.java
+++ msp-gw-web-test1/src/main/java/awesomeCoffee/dto/OrderlistDTO.java
@@ -8,61 +8,65 @@ public class OrderlistDTO {
 
 	private String orderPrice;
 	private String goodsName;
+	private String goodsPrice;
+	private String price;
+	
 	
 	@Override
 	public String toString() {
 		return "OrderlistDTO [orderNum=" + orderNum + ", goodsNum=" + goodsNum + ", orderlistQty=" + orderlistQty
-				+ ", orderlistSum=" + orderlistSum + ", orderPrice=" + orderPrice + ", goodsName=" + goodsName + "]";
+				+ ", orderlistSum=" + orderlistSum + ", orderPrice=" + orderPrice + ", goodsName=" + goodsName
+				+ ", goodsPrice=" + goodsPrice + ", price=" + price + "]";
 	}
-
 	public String getOrderNum() {
 		return orderNum;
 	}
-
 	public void setOrderNum(String orderNum) {
 		this.orderNum = orderNum;
 	}
-
 	public String getGoodsNum() {
 		return goodsNum;
 	}
-
 	public void setGoodsNum(String goodsNum) {
 		this.goodsNum = goodsNum;
 	}
-
 	public String getOrderlistQty() {
 		return orderlistQty;
 	}
-
 	public void setOrderlistQty(String orderlistQty) {
 		this.orderlistQty = orderlistQty;
 	}
-
 	public String getOrderlistSum() {
 		return orderlistSum;
 	}
-
 	public void setOrderlistSum(String orderlistSum) {
 		this.orderlistSum = orderlistSum;
 	}
-
 	public String getOrderPrice() {
 		return orderPrice;
 	}
-
 	public void setOrderPrice(String orderPrice) {
 		this.orderPrice = orderPrice;
 	}
-
 	public String getGoodsName() {
 		return goodsName;
 	}
-
 	public void setGoodsName(String goodsName) {
 		this.goodsName = goodsName;
 	}
-
+	public String getGoodsPrice() {
+		return goodsPrice;
+	}
+	public void setGoodsPrice(String goodsPrice) {
+		this.goodsPrice = goodsPrice;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
 	
 	
 	
